f915ff99997f9cb80e78047d8ca4e0d65ccce6cb
Ande5 <33572132+Ande5@users.noreply.github.com>
2018-03-01 15:36:28 +0300

    new commit

--- ConsoleApplication12/Program.cs
+++ ConsoleApplication12/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ццццццццццццц");
+            Console.WriteLine("ssdпсскккпыввываа");
         }
     }
 }
