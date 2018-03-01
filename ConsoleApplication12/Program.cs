9e2ff375672398ea65bf55387087833a7d141eb5
Ande5 <33572132+Ande5@users.noreply.github.com>
2018-03-01 15:14:58 +0300

    sds

--- ConsoleApplication12/Program.cs
+++ ConsoleApplication12/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ssdпсскккпыввываа");
+            Console.WriteLine("ццццццццццццц");
         }
     }
 }
